46fb5abd9efc5af52c17dffd652e28f582886660
aiv <116391159+azaiv@users.noreply.github.com>
2022-11-22 19:37:05 +0300

    First controller enjoyed

--- SpotifyPrototype/Controllers/StartedController.swift
+++ SpotifyPrototype/Controllers/StartedController.swift
@@ -7,9 +7,98 @@
 
 import UIKit
 
-class StartedController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .black
+class StartedController: BaseController {
+    private let backgroundImage: UIImageView = {
+        let image = UIImageView()
+        image.image = R.BackgroundImage.first
+        image.contentMode = .scaleAspectFill
+        return image
+    }()
+    
+    private let logo: UIImageView = {
+        let image = UIImageView()
+        image.image = R.Icons.logo
+        return image
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = R.Text.GetStarted.titleText
+        label.textColor = R.Colors.white
+        label.font = R.Fonts.bold(with: 22)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let textInfo: UILabel = {
+        let label = UILabel()
+        label.text = R.Text.GetStarted.textInfo
+        label.textColor = R.Colors.grey
+        label.font = R.Fonts.bold(with: 16)
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private let button: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = R.Colors.salt
+        button.setTitle(R.Text.GetStarted.getStarted, for: .normal)
+        button.titleLabel?.font = R.Fonts.bold(with: 22)
+        button.titleLabel?.textColor = R.Colors.white
+        button.layer.cornerRadius = 35
+        button.addTarget(self, action: #selector(getNextController), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc func getNextController() {
+        let nextController = ChooseController()
+        nextController.modalPresentationStyle = .overCurrentContext
+        self.present(nextController, animated: true)
+    }
+}
+
+extension StartedController {
+    override func setupViews() {
+        super.setupViews()
+        view.setupView(backgroundImage)
+        view.setupView(logo)
+        view.setupView(titleLabel)
+        view.setupView(textInfo)
+        view.setupView(button)
+    }
+    
+    override func constraintViews() {
+        super.constraintViews()
+        NSLayoutConstraint.activate([
+            backgroundImage.topAnchor.constraint(equalTo: view.topAnchor),
+            backgroundImage.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            backgroundImage.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            backgroundImage.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            logo.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 15),
+            logo.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            titleLabel.bottomAnchor.constraint(equalTo: textInfo.topAnchor, constant: -15),
+            
+            textInfo.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            textInfo.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 45),
+            textInfo.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -45),
+            textInfo.bottomAnchor.constraint(equalTo: button.topAnchor, constant: -30),
+            
+            button.heightAnchor.constraint(equalToConstant: 70),
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            button.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40),
+            button.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -15),
+        ])
+        
+    }
+    
+    override func configureApperance() {
+        super.configureApperance()
+        view.backgroundColor = R.Colors.black
+        button.makeSystem(button)
     }
 }
--- SpotifyPrototype/Resources/Resources.swift
+++ SpotifyPrototype/Resources/Resources.swift
@@ -8,6 +8,30 @@
 import UIKit
 
 enum R {
+    enum Icons {
+        static let logo = UIImage(named: "SpotifyLogo")
+    }
+    
+    enum Colors {
+        static let salt = UIColor(hexString: "#42C83C")
+        static let white = UIColor(hexString: "#DADADA")
+        static let black = UIColor(hexString: "#1C1B1B")
+        static let grey = UIColor(hexString: "#797979")
+    }
+    
+    enum BackgroundImage {
+        static let first = UIImage(named: "first")
+        static let second = UIImage(named: "second")
+    }
+    
+    enum Text {
+        enum GetStarted {
+            static let getStarted = "Get Started"
+            static let titleText = "Enjoy Listening To Music"
+            static let textInfo = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sagittis enim purus sed phasellus. Cursus ornare id scelerisque aliquam."
+        }
+    }
+    
     enum Fonts {
         static func regular(with size: CGFloat) -> UIFont {
             UIFont(name: "Satoshi-Regular", size: size) ?? UIFont()
